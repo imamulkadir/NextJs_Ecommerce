a57dc5b724a79da931815dbca5c8012c781e9f8f
Imamul Kadir <imamulkadiroyan@gmail.com>
2023-11-21 16:03:42 +0600

    Added actions, updated addtocartbutton

--- src/app/products/[id]/AddToCartButton.tsx
+++ src/app/products/[id]/AddToCartButton.tsx
@@ -1,15 +1,27 @@
 "use client";
 
+import { useState, useTransition } from "react";
+
 interface AddToCartButtonProps {
     productId: string,
+    incrementProductQuantity: (productId: string) => Promise<void>,
 }
 
-export default function AddToCartButton({productId} : AddToCartButtonProps) {
+export default function AddToCartButton({productId, incrementProductQuantity} : AddToCartButtonProps) {
+    const [isPending, startTransition] = useTransition();
+    const [success, setSuccess] = useState(false);
+
     return (
         <div className="flex items-center gap-2">
             <button 
             className="btn btn-primary"
-            onClick={() => {}}
+            onClick={() => {
+                setSuccess(false);
+                startTransition(async () => {
+                    await incrementProductQuantity(productId);
+                    setSuccess(true);
+                })
+            }}
             >
                 Add to Cart
                 <svg 
@@ -27,6 +39,10 @@ export default function AddToCartButton({productId} : AddToCartButtonProps) {
                         />
                 </svg>
             </button>
+            {isPending && <span className="loading loading-spinner loading-md"/>}
+            {!isPending && success && (
+                <span className="text-success">Added to Cart.</span>
+            )}
         </div>
     )
 }
